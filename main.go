3c9f9d5a5779a1172f897ad915e3aed485235600
Forrest <Forrestmillerj@gmail.com>
2020-08-25 21:54:11 -0500

    delete handler

--- main.go
+++ main.go
@@ -40,14 +40,29 @@ func main() {
 	router.Use(static.Serve("/", static.LocalFile("./angular-tour-of-heroes/dist/angular-tour-of-heroes", true)))
 
 	router.GET("/api/heroes", heroHandler)
+	router.POST("/api/heroes", addHandler)
 	router.PUT("/api/heroes", updateHandler)
 	router.GET("/api/heroes/:id", detailHandler)
+	// router.DELETE("/api/heroes/:id", deleteHandler)
 	router.Run(":8080")
 }
 
 // return all heroes
 func heroHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, Heroes)
+}
+
+func addHandler(c *gin.Context) {
+	body := c.Request.Body
+	// create hero strucy to decode to
+	newHero := Hero{}
+	// decode body
+	json.NewDecoder(body).Decode(&newHero)
+	newHero.Id = len(Heroes) + 1
+	fmt.Println("New hero", newHero)
+	Heroes = append(Heroes, newHero)
+	fmt.Println("all heros", Heroes)
+	c.JSON(http.StatusOK, newHero)
 }
 
 // return a single hero
@@ -66,6 +81,17 @@ func detailHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, hero)
 }
 
+// // Delete handler
+// func deleteHandler(c *gin.Context) {
+// 	p := c.Param("id")
+// 	matchId, _ := strconv.Atoi(p)
+// 	for i, _ := range Heroes {
+// 		if Heroes[i].Id == matchId {
+// 			Heroes = RemoveIndex(Heroes, i)
+// 		}
+// 	}
+}
+
 // update hero name
 func updateHandler(c *gin.Context) {
 	// parse request body
@@ -80,4 +106,8 @@ func updateHandler(c *gin.Context) {
 			Heroes[i].Name = hero.Name
 		}
 	}
+}
+
+func RemoveIndex(s []Hero, index int) []Hero {
+	return append(s[:index], s[index+1:]...)
 }
